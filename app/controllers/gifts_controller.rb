60482b9e1cc180babee292c5d4b0c5879e3bc6a0
Alex Hazoury <a.hazoury@gmail.com>
2013-04-01 14:22:24 +0100

    added edit update and destroy controllers

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -22,11 +22,30 @@ class GiftsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:list_id])
+    @gift = @list.gifts.find(params[:id])
   end
 
   def update
+    @list = List.find(params[:list_id])
+    @gift = @list.gifts.find(params[:id])
+    if @gift.update_attributes(params[:gift])
+      flash[:notice] = "Your changes have been saved."
+      redirect_to list_path(@list)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @list = List.find(params[:list_id])
+    @gift = @list.gifts.find(params[:id])
+    if @gift.delete
+      flash[:notice] = 'Your gift has been deleted.'
+      redirect_to list_path(@list)
+    else
+      flash[:notice] = 'You have a problem.  Try again!'
+      redirect_to list_path(@list)
+    end
   end
 end
